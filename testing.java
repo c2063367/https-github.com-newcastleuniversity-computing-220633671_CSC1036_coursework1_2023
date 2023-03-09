eeeb31c326ed4da00763c97c897533b067748ae0
c2063367 <114020033+c2063367@users.noreply.github.com>
2023-03-09 12:38:05 +0000

    Delete testing.java

--- testing.java
+++
@@ -1,9 +0,0 @@
-public class testing {
-    public static void main(String[] args) {
-        Item Item1 = new Item(1, "Furniture","KK",5000.00, 2003 );
-        Item Item2 = new Item(2, "Sculpture","C",100.00, 1999 );
-        Item Item3 = new Item(3, "Painting","D",5200.00, 1899 );
-        Item Item4 = new Item(4, "Carpet","L",1000.00, 2020 );
-        Item Item5 = new Item(5, "Lamp","Z",5.00, 2000 );
-    }
-}
